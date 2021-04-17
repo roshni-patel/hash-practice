2acd014414b86c5d04a877166370158717152173
roshni-patel <rpatel01@wesleyan.edu>
2021-04-17 13:48:01 -0500

    Required methods passing, need to add time and space complexities

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,19 +1,46 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: 
+# Space Complexity: 
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  return strings if strings.length <= 1 
+  grouped = Hash.new
+
+  strings.each do |word|
+    sorted = word.chars.sort!
+    if grouped[sorted].nil? 
+      grouped[sorted] = [word]
+    else
+      grouped[sorted] << word 
+    end
+  end
+  return grouped.values 
 end
+
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: 
+# Space Complexity: 
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  result = []
+  return result if list.empty? 
+  frequencies = Hash.new(0)
+
+  list.each do |element|
+    frequencies[element] += 1 
+  end
+
+  sorted = frequencies.sort_by { |element, freq| -freq }
+  i = 0 
+  k.times do 
+    result << sorted[i][0]
+    i += 1
+  end
+
+  return result 
 end
 
 
